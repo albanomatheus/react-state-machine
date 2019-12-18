a8c08c0403e0bbc7ab211402539a8fa6549aff37
albano <albanomatheus16@gmail.com>
2019-12-18 17:55:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import { useMachine } from "@xstate/react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { Machine, assign } from "xstate";
+import "./App.css";
+
+const foo = {
+	email: "foo",
+	password: "foo123"
+};
+
+function userLogin(user) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			user.email === foo.email && user.password === foo.password
+				? resolve(`Welcome ${user.username}`)
+				: reject("Username or password invalid!");
+		}, 2000);
+	});
+}
+
+const stateMachine = Machine({
+	initial: "initial",
+	context: {
+		msg: ""
+	},
+	states: {
+		initial: {
+			on: {
+				SUBMIT: [
+					{
+						target: "loading",
+						cond: (ctx, event) => {
+							return event.data.email !== "" && event.data.password !== "";
+						}
+					},
+					{
+						target: "error",
+						actions: assign({ msg: (ctx, event) => "teste" })
+					}
+				]
+			}
+		},
+		loading: {
+			invoke: {
+				id: "login",
+				src: (ctx, event) => userLogin({ ...event.data }),
+				onDone: {
+					target: "logged",
+					actions: assign({ msg: (ctx, event) => event.data })
+				},
+				onError: {
+					target: "error",
+					actions: assign({ msg: (ctx, event) => event.data })
+				}
+			}
+		},
+		error: {
+			on: {
+				SUBMIT: {
+					target: "loading",
+					cond: (ctx, event) => {
+						return event.data.email !== "" && event.data.password !== "";
+					}
+				}
+			}
+		},
+		logged: {
+			type: "final"
+		}
+	}
+});
+
+const Form = props => {
+	const [form, updateForm] = useState({
+		email: "",
+		password: ""
+	});
+
+	return (
+		<form
+			onSubmit={e => {
+				e.preventDefault();
+				props.send({ type: "SUBMIT", data: { ...form } });
+			}}
+		>
+			<div className='form-group'>
+				<label>Email</label>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='Enter email'
+					value={form.email}
+					onChange={e => updateForm({ ...form, email: e.target.value })}
+				/>
+			</div>
+			<div className='form-group'>
+				<label>Password</label>
+				<input
+					type='password'
+					className='form-control'
+					placeholder='Password'
+					value={form.password}
+					onChange={e => updateForm({ ...form, password: e.target.value })}
+				/>
+			</div>
+			<button type='submit' className='form-control btn btn-success'>
+				Submit
+			</button>
+		</form>
+	);
+};
+
+const ErrorMessage = props => {
+	return props.machine.matches("error") ? (
+		<div className='alert error'>
+			{props.machine.context.msg
+				? props.machine.context.msg
+				: "Oh no! No error message."}
+		</div>
+	) : null;
+};
+
+const App = () => {
+	const [machine, send] = useMachine(stateMachine);
+
+	return (
+		<div style={{ margin: "130px auto", width: "800px" }}>
+			<MicroFlow machineValue={machine.value} />
+			<ErrorMessage machine={machine} />
+			<Form send={send} />
+		</div>
+	);
+};
+
+const MicroFlowState = props => {
+	return (
+		<button
+			className={`state btn ${
+				props.machineValue === props.name ? "btn-danger" : "btn-primary"
+			}`}
+		>
+			{props.name}
+		</button>
+	);
+};
+
+const MicroFlow = props => {
+	const states = Object.keys(stateMachine.states).map(state => {
+		return (
+			<MicroFlowState
+				name={state}
+				key={state}
+				machineValue={props.machineValue}
+			/>
+		);
+	});
+
+	return <div style={{ textAlign: "center" }}>{states}</div>;
+};
+
+export default App;
